6bf11f63a58fd5a423b95f254684af655bd2abb7
Rajiv Srivastava <itsrajiv.srivastava@gmail.com>
2021-07-15 12:35:39 +0530

    Delete order-event-driven-service directory

--- order-event-driven-service/src/main/java/com/online/store/demo/ordereventdriven/stream/OrderEventProducerStreamConfig.java
+++
@@ -1,8 +0,0 @@
-package com.online.store.demo.ordereventdriven.stream;
-
-import org.springframework.cloud.stream.annotation.EnableBinding;
-
-@EnableBinding(OrderEventProducerStream.class)
-public class OrderEventProducerStreamConfig {
-
-}
